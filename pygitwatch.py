3e89d1c7c1b38ca6a8aa11b15d1263cebb90582d
Simon S. Andersen <syrlander98@gmail.com>
2018-02-26 17:43:35 +0100

    api handler is done

--- pygitwatch.py
+++ pygitwatch.py
@@ -29,9 +29,7 @@ def send_notification(title, content):
     resp = os.system(base_command + " '" + title + "' " + "'" + content + "'")
 
     # 0 == successfully executed
-    if (resp == 0):
-        return True
-    return False
+    return not bool(resp)
 
 
 def get_input_string(msg):
@@ -43,12 +41,6 @@ def get_input_string(msg):
         sys.exit()
 
 
-def kill_app(msg=""):
-    """Prints exit message, then kills application"""
-    print(msg)
-    sys.exit()
-
-
 class Github_ApiHandler(object):
     """Class holds a login session for a given github account"""
 
@@ -56,6 +48,12 @@ class Github_ApiHandler(object):
         """Class constructor"""
         # Default Github api base url
         self.base_url = base_url
+
+        # Github username
+        self.username = ""
+
+        # Remaining rate limit default, 1 for auth request
+        self.remaining_rate_limit = 2
 
         # Prompt for account access token, if non given
         if access_token:
@@ -67,41 +65,80 @@ class Github_ApiHandler(object):
         # Validate access token (auth)
         if self.__auth_validate():
             print("Valid access token!")
-        else:
-            kill_app("Error: Invalid access token!")
 
-    def __send_get(self, url, payload=""):
+            # Set current rate limit
+            self.__set_rate_limit()
+
+            # Set username
+            self.__set_username()
+        else:
+            raise Exception("Error: Invalid access token!")
+
+    def __send_get(self, url, payload={}):
         """Sends a get request using the given url and optional payload"""
+        # Append the access token to each get request
+        payload['access_token'] = self.access_token
+
         try:
-            return requests.get(url, params=payload)
+            if self.remaining_rate_limit > 0:
+                # Decrement rate limit
+                self.remaining_rate_limit -= 1
+                return requests.get(url, params=payload)
+            else:
+                raise Exception("Error: Rate limit exceeded or not set, please wait until the next hourly reset.")
         except ConnectionError:
-            kill_app("Error: Unable to connect to Github!")
+            raise Exception("Error: Unable to connect to Github!")
 
     def __auth_validate(self):
-        """Try to authenticate the access token
+        """Try to authenticate the access token, using basic auth
             Returns: True (success, valid access token)
                      False (failure, invalid access token)"""
-        payload = {'access_token': self.access_token}
-
-        res = self.__send_get(self.base_url, payload=payload)
+        res = self.__send_get(self.base_url)
 
         return (res.status_code == 200)
+
+    def __set_username(self):
+        """Sets the username corresponding to the given access token"""
+        res = self.__send_get(self.base_url + "user")
+
+        # Find the username via. the json respons
+        if res.status_code == 200:
+            try:
+                self.username = res.json()['login']
+            except KeyError:
+                raise Exception("Error: Unable to retrieve username! (access token possibly configured incorrectly)")    
+        else:
+            raise Exception("Error: Unable to connect to github!")
+
+    def __set_rate_limit(self):
+        """Sets the remaining rate limit"""
+        res = self.__send_get(self.base_url + "rate_limit")
+
+        if res.status_code == 200:
+            self.remaining_rate_limit = res.json()['rate']['remaining']
+        else:
+            raise Exception("Error: Unable to connect to Github!")
 
     def get_repos(self):
-        """Gets the list of current events"""
-        payload = {'access_token': self.access_token}
+        """Returns a list of all repos for the authenticated user"""
+        res = self.__send_get(self.base_url + "user/repos")
 
-        res = self.__send_get(self.base_url + "user/repos", payload=payload)
+        if res.status_code == 200:
+            return res.json()
+        else:
+            raise Exception("Error: Unable to retrieve repos! (access token possibly configured incorrectly)")
 
-        # Test print
-        for item in res.json():
-            print(item, end="\n\n")
+    def get_repo_events(self, username, repo_name):
+        """Returns all latest events from repo"""
+        res = self.__send_get(self.base_url + "repos/" + username + "/" + repo_name + "/events")
 
-        return (res.status_code == 200)
+        if res.status_code == 200:
+            return res.json()
+        raise Exception("Error: Invalid username or repository name.")
 
 
 class RepeatedTimer(object):
-    """Timer class which executes a given function every interval"""
+    """Timer class which executes a given function every interval (seconds)"""
 
     def __init__(self, func, interval, *args, **kwargs):
         self.scheduler = sched.scheduler(time.time, time.sleep)
@@ -131,7 +168,15 @@ def Main():
     # Test token: 53ef1c95a035a7e7084077ce8dd2ec5af04123fb
     github_session = Github_ApiHandler(
         access_token="53ef1c95a035a7e7084077ce8dd2ec5af04123fb")
-    print(github_session.get_repos())
+
+    # Get list of authenticated users repos
+    repos = github_session.get_repos()
+    print(repos)
+
+    # Get repo events test
+    # resp = github_session.get_repo_events("Nephz", "interaktionsdesign")
+    # for item in resp:
+    #     print(item, end="\n\n")
 
     # Test of the RepeatedTimer
     # def hello(name):
